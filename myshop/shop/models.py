667c3bb22195cde2cf905c879835da01528fb8ee
spacemonkeythe <mladen.meseldzija@gmail.com>
2017-05-18 20:19:46 +0200

    Implement Product views - list and detail.

--- myshop/shop/models.py
+++ myshop/shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200,
@@ -13,7 +14,11 @@ class Category(models.Model):
         verbose_name_plural = 'categories'
 
     def __str__(self):
-        return name
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_list_by_category',
+                        args=[self.slug])
 
 class Product(models.Model):
     category = models.ForeignKey(Category,
@@ -36,3 +41,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail',
+                    args=[self.id, self.slug])
